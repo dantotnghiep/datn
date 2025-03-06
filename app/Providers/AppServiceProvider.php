8162235dfd306d8ecc6ef2b4d1c192c8549942d5
yeumaithanh <phamthanhtq2004@gmail.com>
2025-03-06 17:12:56 +0700

    m

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use App\Models\Cart;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,16 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('client.layouts.partials.lelf-navbar', function ($view) {
-            $cartItems = Cart::all();
-            $cartTotal = $cartItems->sum(function ($item) {
-                return $item->getFinalPriceAttribute() * $item->quantity;
-            });
-
-            $view->with([
-                'cartItems' => $cartItems,
-                'cartTotal' => $cartTotal
-            ]);
-        });
+        //
     }
 }
