f21c2706ab926e31c775d3968e8742d5db7da099
dothanhdat <dothanhdat20044@gmail.com>
2025-04-17 01:56:48 +0700

    Update: chỉnh sửa giao diện tiếng Việt

--- resources/views/admin/attribute/attribute-edit.blade.php
+++ resources/views/admin/attribute/attribute-edit.blade.php
@@ -1,17 +1,17 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <!-- main content -->
+    <!-- Nội dung chính -->
     <div class="cr-main-content">
         <div class="container-fluid">
-            <!-- Page title & breadcrumb -->
+            <!-- Tiêu đề trang & breadcrumb -->
             <div class="cr-page-title cr-page-title-2">
                 <div class="cr-breadcrumb">
-                    <h5>Attribute</h5>
+                    <h5>Thuộc tính</h5>
                     <ul>
                         <li><a href="index.html">Carrot</a></li>
-                        <li>Attribute</li>
-                        <li>Attribute</li>
+                        <li>Thuộc tính</li>
+                        <li>Thuộc tính</li>
                     </ul>
                 </div>
             </div>
@@ -36,13 +36,13 @@
                             <div class="cr-cat-list cr-card card-default mb-24px">
                                 <div class="cr-card-content">
                                     <div class="cr-cat-form">
-                                        <h3>Edit Attribute</h3>
+                                        <h3>Chỉnh sửa thuộc tính</h3>
 
                                         <form action="{{ route('admin.attribute.update', $attribute->id) }}" method="POST">
                                             @csrf
                                             @method('PUT')
                                             <div class="form-group">
-                                                <label>Attribute Name</label>
+                                                <label>Tên thuộc tính</label>
                                                 <div class="col-12">
                                                     <input id="name" name="name"
                                                         class="form-control here slug-title" type="text"
@@ -60,7 +60,7 @@
 
                                             <div class="row">
                                                 <div class="col-12 d-flex">
-                                                    <button type="submit" class="cr-btn-primary">Cập Nhập</button>
+                                                    <button type="submit" class="cr-btn-primary">Cập nhật</button>
                                                 </div>
                                             </div>
 
@@ -79,9 +79,9 @@
                                 <table id="cat_data_table" class="table">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Attribute Values</th>
-                                            <th>Action</th>
+                                            <th>Tên</th>
+                                            <th>Giá trị thuộc tính</th>
+                                            <th>Hành động</th>
                                         </tr>
                                     </thead>
 
@@ -90,15 +90,9 @@
                                             <tr>
                                                 <td>{{ $attribute->name }}</td>
                                                 <td>
-                                                    {{-- <span class="cr-sub-cat-list">
-                                                    <span class="cr-sub-cat-count"
-                                                        title="Total Number Attribute Values">5</span>
-                                                    <span class="cr-sub-cat-tag">T-shirt</span>
-                                                    <span class="cr-sub-cat-tag">Shirt</span>
-                                                    <span class="cr-sub-cat-tag">Dress</span>
-                                                    <span class="cr-sub-cat-tag">Jeans</span>
-                                                    <span class="cr-sub-cat-tag">Top</span>
-                                                </span> --}}
+                                                    {{-- 
+                                                    Ví dụ có thể thêm giá trị ở đây nếu muốn
+                                                    --}}
                                                 </td>
                                                 <td>
                                                     <div class="dropdown">
@@ -106,15 +100,13 @@
                                                             style="border: none;padding: 15px 30px;font-size: 20px;background-color: white;"
                                                             data-bs-toggle="dropdown" aria-haspopup="true"
                                                             aria-expanded="false" data-display="static">
-
                                                         </button>
 
-
                                                         <div class="dropdown-menu">
-                                                            <!-- Nút Edit -->
-                                                            <a class="dropdown-item" href="#">Edit</a>
+                                                            <!-- Nút Sửa -->
+                                                            <a class="dropdown-item" href="#">Chỉnh sửa</a>
 
-                                                            <!-- Nút Delete -->
+                                                            <!-- Nút Xoá -->
                                                             <form
                                                                 action="{{ route('admin.attribute.destroy', $attribute->id) }}"
                                                                 method="POST" class="dropdown-item p-0 m-0">
@@ -122,9 +114,8 @@
                                                                 @method('DELETE')
                                                                 <button type="submit"
                                                                     class="dropdown-item"
-                                                                    
                                                                     onclick="return confirm('Bạn có chắc chắn muốn xóa thuộc tính này?')">
-                                                                    Delete
+                                                                    Xóa
                                                                 </button>
                                                             </form>
                                                         </div>
