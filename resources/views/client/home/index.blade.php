34138778745d0e575cb6caae869f3410c97ec3a0
dvquang2003 <dvquang07112003@gmail.com>
2025-05-04 16:41:18 +0700

    a

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -1,56 +1,45 @@
 @extends('client.master')
 @section('content')
-<section class="py-0 px-xl-3">
-    <div class="container px-xl-0 px-xxl-3">
-        <div class="row g-3 mb-9">
-            <div class="col-12">
-                <div class="whooping-banner w-100 rounded-3 overflow-hidden">
-                    <div class="bg-holder z-n1 product-bg"
-                        style="background-image:url({{ asset('theme/prium.github.io/phoenix/v1.22.0/assets/img/e-commerce/whooping_banner_product.png') }});background-position: bottom right;">
+    <section class="py-0 px-xl-3">
+        <div class="container px-xl-0 px-xxl-3">
+            <div class="row g-3 mb-9">
+                <div class="col-12">
+                    <div class="whooping-banner rounded-3 overflow-hidden" style="width: 100%; height: 475px;">
+                        <div class="bg-holder z-n1 product-bg"
+                            style="background-image:url({{ asset('storage/banner/banner.jpg') }});">
+                        </div>
                     </div>
-                    <!--/.bg-holder-->
-                    <div class="bg-holder z-n1 shape-bg"
-                        style="background-image:url({{ asset('theme/prium.github.io/phoenix/v1.22.0/assets/img/e-commerce/whooping_banner_shape_2.png') }});background-position: bottom left;">
-                    </div>
-                    <!--/.bg-holder-->
-                    <div class="banner-text" data-bs-theme="light">
-                        <h2 class="text-warning-light fw-bolder fs-lg-3 fs-xxl-2">Whooping <span
-                                class="gradient-text">60% </span>Off</h2>
-                        <h3 class="fw-bolder fs-lg-5 fs-xxl-3 text-white">on everyday items</h3>
-                    </div><a class="btn btn-lg btn-primary rounded-pill banner-button" href="#!">Shop
-                        Now</a>
                 </div>
             </div>
+
+            <!-- Sản phẩm HOT -->
+            @include('client.home.hot-product')
+
+            <!-- Sản phẩm thường -->
+            @include('client.home.normal-product')
         </div>
-        
-        <!-- Sản phẩm HOT -->
-        @include('client.home.hot-product')
-        
-        <!-- Sản phẩm thường -->
-        @include('client.home.normal-product')
-    </div>
-</section>
+    </section>
 @endsection
 
 @push('scripts')
-<script>
-    $(document).ready(function() {
-        // ToastR setup
-        toastr.options = {
-            "closeButton": true,
-            "newestOnTop": true,
-            "progressBar": true,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "2000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
-        };
-    });
-</script>
+    <script>
+        $(document).ready(function() {
+            // ToastR setup
+            toastr.options = {
+                "closeButton": true,
+                "newestOnTop": true,
+                "progressBar": true,
+                "positionClass": "toast-top-right",
+                "preventDuplicates": false,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "2000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+        });
+    </script>
 @endpush
